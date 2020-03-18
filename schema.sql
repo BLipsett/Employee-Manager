23426d50d46bf75a8225c4a69fdf8f9eb40d41c3
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-03-17 21:31:30 -0400

    employee_db

---
+++ schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS employee_DB;
+CREATE database employee_DB;
+
+USE employee_DB;
+
+CREATE TABLE employee (
+  id INT NOT NULL,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  roles_id INT NOT NULL,
+  manager_id INT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+  id INT NOT NULL,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,4) NULL,
+  department_id INT NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE department (
+  id INT NOT NULL,
+  deartment_name VARCHAR (30) NOT NULL,
+  PRIMARY KEY (id)
+);
