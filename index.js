b82b1862f063551288b5d9ee7a4a66cff609999b
BLipsett <53913632+BLipsett@users.noreply.github.com>
2020-03-22 17:14:46 -0400

    new prompt

--- index.js
+++ index.js
@@ -18,37 +18,12 @@ let connection = mysql.createConnection({
 
 connection.connect(function (err) {
   if (err) throw err;
-  listHouse();
-  
+  //listHouse();
+  runSearch();
+
 });
 
-function listEmps() {
-  connection.query("SELECT * FROM employee;", function (err, data) {
-    if (err) {
-      throw err;
-    }
-    console.log(data);
-    console.log("first")
-  });
-}
 
-function listRoles() {
-  connection.query("SELECT * FROM roles;", function (err, data) {
-    if (err) {
-      throw err;
-    }
-    console.log(data);
-  });
-}
-
-function listHouse() {
-  connection.query("SELECT first_name, last_name, salary, title FROM employee JOIN roles ON employee.id = roles.id;", function (err, data) {
-    if (err) {
-      throw err;
-    }
-    console.table(data);
-  });
-}
 
 
 
@@ -63,22 +38,209 @@ function runSearch() {
         message: "What would you like to do?",
         choices: [
           "View all employees",
-          "Find all artists who appear more than once",
-          "Find data within a specific range",
-          "Search for a specific song",
-          "Find artists with a top song and top album in the same year"
+          "View employees by manager",
+          "View employees by department",
+          "Add employee",
+          "Remove employee",
+          "Update employee role",
+          "Update employee manager",
+          "View all roles"
+
         ]
       }
-    ]).then(answers => {
-      console.log(answers.action);
-      let newPass = new EmployeeBuilder(answers)
+    ]).then(function (answer) {
+      switch (answer.action) {
+        case "View all employees":
+          listHouse();
+          break;
 
+        case "View employees by manager":
+          employeeManager();
+          break;
 
-      console.log(`Your new password is ${newPass.value}`)
+        case "View employees by department":
+          employeeDepartment();
+          break;
 
+        case "Add employee":
+          employeeAdd();
+          break;
+
+        case "Find artists with a top song and top album in the same year":
+          songAndAlbumSearch();
+          break;
+      }
     });
 }
 
+function employeeManager() {
+  connection.query("SELECT first_name, last_name FROM employee;", function (err, data) {
+    if (err) {
+      throw err;
+    }
+    var resultArray = Object.values(JSON.parse(JSON.stringify(data)));
+
+    for (key in resultArray) {
+      if (resultArray.hasOwnProperty(key)) {
+        var value = resultArray[key];
+        //do something with value;
+
+        let fullName = `${value.first_name} ${value.last_name}`
+        console.log(fullName)
+
+        return inquirer
+
+          .prompt({
+            name: "manager",
+            type: "rawlist",
+            message: "Whomst management team are you looking for?",
+            choices: "cheese"
+          })
+          .then(function (answer) {
+            console.log(answer)
+
+          });
+      }
+    }
+  });
+}
+function employeeDepartment() {
+
+  return inquirer
+
+    .prompt({
+      name: "manager",
+      type: "rawlist",
+      message: "Whomst management team are you looking for?",
+      choices: "cheese"
+    })
+    .then(function (answer) {
+      console.log(answer)
+
+      runSearch();
+    });
+}
+
+function artistSearch() {
+  inquirer
+    .prompt({
+      name: "artist",
+      type: "input",
+      message: "What artist would you like to search for?"
+    })
+    .then(function (answer) {
+      var query = "SELECT position, song, year FROM top5000 WHERE ?";
+      connection.query(query, { artist: answer.artist }, function (err, res) {
+        for (var i = 0; i < res.length; i++) {
+          console.log("Position: " + res[i].position + " || Song: " + res[i].song + " || Year: " + res[i].year);
+        }
+        runSearch();
+      });
+    });
+}
+
+function listEmps() {
+  connection.query("SELECT first_name, last_name FROM employee;", function (err, data) {
+    if (err) {
+      throw err;
+    }
+    var resultArray = Object.values(JSON.parse(JSON.stringify(data)));
+
+    for (key in resultArray) {
+      if (resultArray.hasOwnProperty(key)) {
+        var value = resultArray[key];
+        //do something with value;
+
+        let fullName = `${value.first_name} ${value.last_name}`
+        console.log(fullName)
+      }
+    }
+    runSearch();
+  });
+}
+
+function employeeAdd() {
+
+  return inquirer
+
+    .prompt([
+      {
+        name: "first",
+        type: "input",
+        message: "What is the employees first name?"
+      },
+      {
+        name: "last",
+        type: "input",
+        message: "Last name?"
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "Salary?",
+        // validate: (function (value) {
+
+        // })
+      },
+      {
+        name: "title",
+        type: "input",
+        message: "Job title?"
+      },
+      {
+        name: "department",
+        type: "input",
+        message: "Department?"
+      }
+    ]).then(answers => {
+      console.log(answers);
+      createEmployee(answers)
+
+    })
+    .catch(error => {
+      if (error.isTtyError) {
+
+      } else {
+
+      }
+    });
+
+}
+
+function createEmployee(answers) {
+  console.log("Inserting a new employee...\n");
+  let query = connection.query(
+      "INSERT INTO employee SET ?",
+      {
+          first_name: `${answers.first}`,
+          last_name: `${answers.last}`,
+          // salary: `${answers.salary}`,
+          // title: `${answers.title}`,
+          // deartment: `${answers.department}`
+      },
+      function (err, res) {
+          if (err) throw err;
+          console.log(res.affectedRows + " product inserted!\n");
+          // Call updateProduct AFTER the INSERT completes
+
+      }
+  );
+
+  // logs the actual query being run
+  console.log(query.sql);
+}
+
+
+
+function listHouse() {
+  connection.query("SELECT first_name, last_name, salary, title FROM employee JOIN roles ON employee.id = roles.id;", function (err, data) {
+    if (err) {
+      throw err;
+    }
+    console.table(data);
+    runSearch();
+  });
+}
 
 // function artistSearch() {
 //   connection.query("INSERT INTO wishes (wish) VALUES (?)", [req.body.wish], function (err, result) {
